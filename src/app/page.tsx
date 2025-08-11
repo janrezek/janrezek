763df90396a33aacb4f5c5cf9d1790ee801442cc
janrezek <janrezek14@icloud.com>
2025-08-11 22:19:50 +0200

    Adjust for mobile devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <div className="flex flex-col gap-10 items-center justify-center">
           <div className="flex flex-col gap-2 items-center justify-center">
-            <h1 className="text-8xl font-bold">Jan Rezek</h1>
-            <p className="text-4xl text-center font-light text-gray-300">
+            <h1 className="text-6xl sm:text-8xl font-bold">Jan Rezek</h1>
+            <p className="text-xl sm:text-4xl text-center font-light text-gray-300">
               <span className="text-red-500">&lt;</span>{" "}
               <TypingPrefix delayMs={2000} text="Junior"/>{" "} Full Stack Developer {" "}
               <span className="text-red-500">&gt;</span>
