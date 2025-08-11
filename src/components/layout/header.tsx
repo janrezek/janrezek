2eb463849df2d81d1a1319afb871d2e6a803d8c7
janrezek <janrezek14@icloud.com>
2025-08-11 22:17:26 +0200

    forgot the header:)

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,19 @@
+"use client";
+import Link from "next/link";
+
+export default function Header() {
+
+    return (
+        <header className="backdrop-blur-md fixed top-0 left-0 right-0 z-50 flex justify-between items-center h-16 px-10">
+            <div className="flex items-center gap-4 w-25 justify-start">
+
+            </div>
+            <div className="flex items-center gap-4 w-50 justify-center text-white">
+                
+            </div>
+            <div className="flex items-center gap-4 w-25 justify-end">
+
+            </div>
+        </header>
+    );
+}
